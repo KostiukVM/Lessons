17841cc5ff87ba315283af04ca243e2cca161c78
KostiukVM <kostiukdsfv@gmail.com>
2023-12-16 15:06:59 +0200

    add home pc

--- test.php
+++ test.php
@@ -1,2 +1,25 @@
 <?php
-echo 123;
+
+class Animals
+{
+    function Nickname()
+    {
+        echo "I have nickname".PHP_EOL;
+    }
+    function say()
+    {
+        echo "I can say".PHP_EOL;
+    }
+}
+
+class Lion extends Animals
+{
+    function property()
+    {
+        echo "I can hunt".PHP_EOL;
+    }
+    function say()
+    {
+        echo 'I say \'roar\''.PHP_EOL;
+    }
+}
